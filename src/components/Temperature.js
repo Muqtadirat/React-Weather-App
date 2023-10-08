0cbb132e704e4d779bd043b6b2643e517b082c13
Muqtadirat <muqtadir.my@gmail.com>
2023-10-08 16:23:47 +0100

    Implemented icon response

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -4,17 +4,21 @@ import "../App.css";
 
 export default function Temperature(props) {
   let [temperature, setTemperature] = useState(null);
+  let [icon, setIcon] = useState("");
 
   const apiKey = "ae5350b6a304ff06o3a36487d5be8a4t";
   const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
 
-  function showTemperature(response) {
+  function handleResponse(response) {
     setTemperature(response.data.temperature.current);
+    setIcon(
+      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    );
   }
 
   axios
     .get(apiUrl)
-    .then(showTemperature)
+    .then(handleResponse)
     .catch((error) => {
       console.error("Error fetching temperature data:", error);
     });
@@ -24,7 +28,7 @@ export default function Temperature(props) {
       <div className="col">
         <div className="col pt-4" id="temperature">
           <p className="mt-4 fs-1">
-            <img src="" alt="" id="icon" />
+            <img src={icon} alt="" id="icon" />
             <span className="temp" id="temperature-value">
               {Math.round(temperature)}
             </span>
