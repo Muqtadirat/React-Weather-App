8321fd7be9d0524e8f0c62008dfad3be45c62d38
Muqtadirat <muqtadir.my@gmail.com>
2023-10-15 13:20:36 +0100

    Added temp conversion

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
-import axios from "axios";
+
+import ApiHandler from "./ApiHandler";
 import "../App.css";
 
 export default function Temperature(props) {
   let [temperature, setTemperature] = useState(null);
   let [icon, setIcon] = useState(null);
 
-  const apiKey = "ae5350b6a304ff06o3a36487d5be8a4t";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+  function toCelsius(event) {
+    event.preventDefault();
 
-  function handleResponse(response) {
-    setTemperature(response.data.temperature.current);
-    setIcon(
-      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
-    );
+    const celsius = Math.round(((temperature - 32) * 5) / 9);
+    setTemperature(celsius);
   }
 
-  axios
-    .get(apiUrl)
-    .then(handleResponse)
-    .catch((error) => {
-      console.error("Error fetching temperature data:", error);
-    });
+  function toFahrenheit(event) {
+    event.preventDefault();
+
+    const fahrenheit = Math.round((temperature * 9) / 5 + 32);
+    setTemperature(fahrenheit);
+  }
+
+  const handleData = (data) => {
+    setTemperature(data.temperature.current);
+    setIcon(
+      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${data.condition.icon}.png`
+    );
+  };
 
   return (
     <div className="Temperature">
+      <ApiHandler
+        apiUrl={`https://api.shecodes.io/weather/v1/current?query=${props.city}&key=ae5350b6a304ff06o3a36487d5be8a4t&units=metric`}
+        onDataFetched={handleData}
+      />
       <div className="col">
         <div className="col pt-4" id="temperature">
           <p className="mt-4 fs-1">
@@ -33,11 +42,15 @@ export default function Temperature(props) {
               {Math.round(temperature)}
             </span>
             <sup className="temp-parameter">
-              <button type="button" className="celsius">
+              <button type="button" className="celsius" onClick={toCelsius}>
                 °C
               </button>{" "}
               |
-              <button type="button" className="fahrenheit">
+              <button
+                type="button"
+                className="fahrenheit"
+                onClick={toFahrenheit}
+              >
                 °F
               </button>
             </sup>
