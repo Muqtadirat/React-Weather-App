9b8ff6b169c36a7f7bf88d14aa1dc032459918f3
Muqtadirat <muqtadir.my@gmail.com>
2023-10-15 11:53:25 +0100

    Created apiHandler

---
+++ src/components/ApiHandler.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function ApiHandler({ apiUrl, onDataFetched }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data);
+        if (onDataFetched) {
+          onDataFetched(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [apiUrl, onDataFetched]);
+
+
+  return null;
+}
